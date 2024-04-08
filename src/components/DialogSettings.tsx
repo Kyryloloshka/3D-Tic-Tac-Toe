0895cd0eaba65c6b4ead1d8a3c61cf42e1b0ce2c
Kyryloloshka <kirill.contrmptuous@gmail.com>
2024-04-08 09:25:40 +0300

    internationalization settings dialog

--- src/components/DialogSettings.tsx
+++ src/components/DialogSettings.tsx
@@ -16,6 +16,7 @@ import { Button } from './ui/button';
 import { Player } from '@/types';
 import { Checkbox } from './ui/checkbox';
 import { Separator } from './ui/separator';
+import { useTranslations } from 'next-intl';
 
 const DialogSettings = ({restartGame}: {restartGame: Function}) => {
   const [open, setOpen] = useState(false);
@@ -28,6 +29,7 @@ const DialogSettings = ({restartGame}: {restartGame: Function}) => {
   const [selectedIsCenterAvailable, setSelectedIsCenterAvailable] = useState(isCenterAvailable);
   const [selectedIsPlayWithBot, setSelectedIsPlayWithBot] = useState(isPlayWithBot);
   const [selectedBotPlayer, setSelectedBotPlayer] = useState(botPlayer);
+  const t = useTranslations("settings");
 
   const submitSettings = (e: any) => {
     e.preventDefault();
@@ -63,21 +65,21 @@ const DialogSettings = ({restartGame}: {restartGame: Function}) => {
       <DialogContent className="sm:max-w-[425px] bg-dark-2 shadow-primary border-primary-500/90">
         <form onSubmit={submitSettings}>
           <DialogHeader>
-            <DialogTitle>Settings of game</DialogTitle>
+            <DialogTitle>{t("title")}</DialogTitle>
           </DialogHeader>
           <div className="grid gap-4 py-4">
             <div className="grid grid-cols-5 items-center gap-2">
               <Label htmlFor="playFor" className=" col-span-4">
-                First move for
+                {t("firstMoveFor")}
               </Label>
               <RadioGroup className="col-span-1" defaultValue={selectedFirstPlayer} onValueChange={handlePlayerChange}>
                 <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="X" id="r1" />
-                  <Label htmlFor="r1">X</Label>
+                  <RadioGroupItem value={Player.X} id="r1" />
+                  <Label htmlFor="r1">{Player.X}</Label>
                 </div>
                 <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="O" id="r2" />
-                  <Label htmlFor="r2">O</Label>
+                  <RadioGroupItem value={Player.O} id="r2" />
+                  <Label htmlFor="r2">{Player.O}</Label>
                 </div>
               </RadioGroup>
               <Separator className="col-span-5 w-full bg-dark-5" />
@@ -85,7 +87,7 @@ const DialogSettings = ({restartGame}: {restartGame: Function}) => {
                 htmlFor="terms"
                 className="  col-span-4"
               >
-                Center Available
+                {t("centerAvailable")}
               </Label>
               <Checkbox defaultChecked={selectedIsCenterAvailable} onCheckedChange={handleCenterAvaliableChange} className='col-span-1' id="terms" />
               <Separator className="col-span-5 w-full bg-dark-5" />
@@ -93,14 +95,14 @@ const DialogSettings = ({restartGame}: {restartGame: Function}) => {
                 htmlFor="bot"
                 className="col-span-4"
               >
-                Game with Bot
+                {t("gameWithBot")}
               </Label>
               <Checkbox defaultChecked={selectedIsPlayWithBot} onCheckedChange={handlePlayWithBotChange} className='col-span-1' id="bot" />
               {selectedIsPlayWithBot && 
                 <>
                 <Separator className="col-span-5 w-full bg-dark-5" />
                   <Label htmlFor="botPlayer" className="col-span-4">
-                    The Bot Plays as
+                  {t("botPlaysAs")}
                   </Label>
                   <RadioGroup className="col-span-1" defaultValue={selectedBotPlayer} onValueChange={handleBotPlayerChange}>
                     <div className="flex items-center space-x-2">
@@ -117,7 +119,7 @@ const DialogSettings = ({restartGame}: {restartGame: Function}) => {
             </div>
           </div>
           <DialogFooter>
-            <Button variant="neon" type="submit">Save and restart</Button>
+            <Button variant="neon" type="submit">{t("saveAndRestart")}</Button>
           </DialogFooter>
         </form>
       </DialogContent>
