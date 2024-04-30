3dbb406d221a7d00d0e95fe420168516d529037d
Kyryloloshka <kirill.contrmptuous@gmail.com>
2024-04-30 22:05:02 +0300

    page about game as well as rules, refactor titles

--- src/app/[locale]/about-game/page.tsx
+++ src/app/[locale]/about-game/page.tsx
@@ -10,9 +10,9 @@ const About = () => {
     <div className={`${rubik.className} overflow-hidden text-light-2 flex-auto flex flex-col min-h-full common-container container-restrictive p-3 md:p-6 gap-5`}>
       <h1 className="text-3xl font-semibold text-shadow-neon text-primary-500 tracking-wide">{t("aboutTitle")} <span className=" whitespace-nowrap">3D Tic Tac Toe</span></h1>
       <p className="leading-6 text-lg tracking-wide pb-3">{t("aboutMessage")}</p>
-      <h3 className="text-2xl font-semibold text-shadow-neon text-primary-500 tracking-wide">{t("visionTitle")}</h3>
+      <h2 className="text-2xl font-semibold text-shadow-neon text-primary-500 tracking-wide">{t("visionTitle")}</h2>
       <p className="leading-6 text-lg tracking-wide pb-3">{t("visionMessage")}</p>
-      <h3 className="text-2xl font-semibold text-shadow-neon text-primary-500 tracking-wide">{t("featuresTitle")}</h3>
+      <h2 className="text-2xl font-semibold text-shadow-neon text-primary-500 tracking-wide">{t("featuresTitle")}</h2>
       <ol className="flex flex-col gap-3 ">
         <li className="tracking-wide"><span className="font-semibold text-shadow-neon text-primary-500">{t("feature1Title")}</span> {t("feature1Description")}</li>
         <li className="tracking-wide"><span className="font-semibold text-shadow-neon text-primary-500">{t("feature2Title")}</span> {t("feature2Description")}</li>
