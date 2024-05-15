9c07b11745cccca3ad9446421cddece8e7d5deaa
Kyryloloshka <kirill.contrmptuous@gmail.com>
2024-05-15 15:50:38 +0300

    fix burger menu with nonanimation whith thransition between pages

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -88,7 +88,7 @@ const Header = () => {
     <header className="z-[500] fade-in top-0 sticky min-h-[40px] md:min-h-[54px] shadow-primary bg-dark-2 gap-3 flex justify-between items-center px-3 md:px-6 py-1 md:py-3">
       <div className="md:hidden z-[502] ml-[-8px]">
         <label className="hamburger" >
-          <input type="checkbox" onClick={() => toggleIsOpenBurgerMenu()}/>
+          <input checked={isOpenBurger} type="checkbox" onClick={() => toggleIsOpenBurgerMenu()}/>
           <svg viewBox="0 0 32 32">
             <path className="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
             <path className="line" d="M7 16 27 16"></path>
